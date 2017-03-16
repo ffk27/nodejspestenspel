865ee6ee4a0c6f8e0ca564147005363a4cc61841
Broodhond <borismurman@gmail.com>
2017-03-16 13:28:26 +0100

    Server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+
+
+http.createServer(function (req, res) {
+
+    
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.end('Hallo, wereld!');
+
+    
+
+}).listen(process.env.PORT || 8000);
