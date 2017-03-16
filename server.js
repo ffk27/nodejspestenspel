f35dcca22c8c6700951d0ed5de9b8f972f9ecded
ffk27 <fpfolkertsma@gmail.com>
2017-03-16 13:53:22 +0100

    n

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
-
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!')
-});
+var http = require('http');
+http.createServer(
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello World\n');
+    }
+).listen(8000);
+console.log('Server running at http://localhost:8000/');
