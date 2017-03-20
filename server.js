7178b7dde9d06ceaa2a4729000e43f638ca48327
Broodhond <borismurman@gmail.com>
2017-03-20 11:00:01 +0100

    Server.js geeft nu index.html terug als response

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    res.end('Hallo, wereld!');
-
-
+    fs = require('fs');
+    var p  = './index.html';
+    fs.readFile(p, function(err, html) {
+        if(err){
+            throw err;
+        }
+        res.writeHead(200 , { "Content-Type": "text/html"});
+        res.write(html);
+        res.end();
+        console.log("response sent..");
+    });
 
 }).listen(process.env.PORT || 8080);
