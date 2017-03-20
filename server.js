4de1c788131edeac490350fc0cb8147e6e11b04b
Broodhond <borismurman@gmail.com>
2017-03-20 13:08:24 +0100

    Server.js aangepast

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 var path    = require("path");
+var port = 8000;
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000!')
+app.listen(port, function () {
+    console.log('Pesten app luistert op port ' + port);
 });
