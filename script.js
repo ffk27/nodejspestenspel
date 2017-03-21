742e43cbc24a4bcbc53081fda6faf8f644873023
Brian <brian_360@live.nl>
2017-03-21 12:38:40 +0100

    Merge fix

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+
+
+function startGame() {
+    //Shuffle deck
+
+    //give cards
+
+    //delete start button
+
+}
+
 function loadDeck() {
     var socket = io.connect('http://localhost:8000');
+
     socket.on('cards', function (data) {
         console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            var pxdown = i * 1;
+        for(var i = 0; i < data.length; i++){
+            var pxdown = 0;
             var pxright = i * 10;//*10 on 8% width
-            $("#content").append(
-                '<img class="card" src="img/cards-svg/' + data[i].card + data[i].type + '.svg" style="margin-top:' + pxdown + 'px;margin-left:' + pxright + 'px;"/>'
+            $("#playercards").append(
+                '<img class="card" id="'+data[i].card+data[i].type+'" draggable="true" ondragstart="drag(event)" ' +
+                'src="img/cards-svg/'+data[i].card+data[i].type+'.svg" ' +
+                'style="margin-top:' + pxdown + 'px;margin-left:' + pxright + 'px;"/>'
             );
         }
         //socket.emit('my other event', { my: 'data' });
